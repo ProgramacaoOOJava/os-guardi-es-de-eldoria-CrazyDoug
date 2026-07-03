58217d7ea76bb04e7f28b6d9b409e68b77b54e8b
Doug <33207031+CrazyDoug@users.noreply.github.com>
2026-07-02 21:01:26 -0300

    update
    NIVEL NOVATO

--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,36 @@
+// Feito por Douglas Alves Costa
+// Nível Novato
+
 public class Personagem {
+
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
 
-    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
-        this.nome = nome;
-        .
-        .
-        .
+    public Personagem(String nome,
+                      String classe,
+                      int nivel,
+                      int pontosDeVida,
+                      double poderBase) {
 
+        this.nome = nome;
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     }
 
     public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
 
+        System.out.println("=====================================");
+        System.out.println("      GUARDIÕES DE ELDORIA");
+        System.out.println("=====================================");
+        System.out.println("Nome: " + nome);
+        System.out.println("Classe: " + classe);
+        System.out.println("Nível: " + nivel);
+        System.out.println("Pontos de Vida: " + pontosDeVida);
+        System.out.println("Poder Base: " + poderBase);
     }
 }
-
